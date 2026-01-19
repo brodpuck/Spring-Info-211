f5bf074b269c44b040474db60c0e7e886d98b01e
brodpuck <brodpuck@iu.edu>
2026-01-19 14:12:07 -0500

    Update Exercise 9.1.java

--- M01/Exercise 9.1.java
+++ M01/Exercise 9.1.java
@@ -1,43 +1,40 @@
 public class Exercise09_01 {
     public static void main(String[] args) {
-        Rectangle rectangle1 = new Rectangle(4, 40);
-        Rectangle rectangle2 = new Rectangle(3.5, 35.9);
+        MyRectangle myRectangle = new MyRectangle(4, 40);
+        System.out.println("The area of a rectangle with width " +
+                myRectangle.width + " and height " +
+                myRectangle.height + " is " +
+                myRectangle.getArea());
+        System.out.println("The perimeter of a rectangle is " +
+                myRectangle.getPerimeter());
 
-        System.out.println("Rectangle 1");
-        System.out.println("Width: " + rectangle1.width);
-        System.out.println("Height: " + rectangle1.height);
-        System.out.println("Area: " + rectangle1.getArea());
-        System.out.println("Perimeter: " + rectangle1.getPerimeter());
-
-        System.out.println();
-
-        System.out.println("Rectangle 2");
-        System.out.println("Width: " + rectangle2.width);
-        System.out.println("Height: " + rectangle2.height);
-        System.out.println("Area: " + rectangle2.getArea());
-        System.out.println("Perimeter: " + rectangle2.getPerimeter());
+        MyRectangle yourRectangle = new MyRectangle(3.5, 35.9);
+        System.out.println("The area of a rectangle with width " +
+                yourRectangle.width + " and height " +
+                yourRectangle.height + " is " +
+                yourRectangle.getArea());
+        System.out.println("The perimeter of a rectangle is " +
+                yourRectangle.getPerimeter());
     }
 }
 
-class Rectangle {
-    double width;
-    double height;
+class MyRectangle {
+    double width = 1;
+    double height = 1;
 
-    Rectangle() {
-        width = 1;
-        height = 1;
+    public MyRectangle() {
     }
 
-    Rectangle(double newWidth, double newHeight) {
+    public MyRectangle(double newWidth, double newHeight) {
         width = newWidth;
         height = newHeight;
     }
 
-    double getArea() {
+    public double getArea() {
         return width * height;
     }
 
-    double getPerimeter() {
+    public double getPerimeter() {
         return 2 * (width + height);
     }
 }
